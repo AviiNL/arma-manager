82211dc66d2310b2926ea8e42e19a4766195e1e4
AviiNL <me@avii.nl>
2023-07-15 20:53:15 +0200

    uploading and processing presets part 1

--- crates/arma/src/parser.rs
+++ crates/arma/src/parser.rs
@@ -50,69 +50,69 @@
 //     Ok(collection)
 // }
 
-// fn get_mod_id(element: &scraper::ElementRef) -> u64 {
-//     // <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=450814997" data-type="Link">https://steamcommunity.com/sharedfiles/filedetails/?id=450814997</a>
-//     let link = element
-//         .select(&scraper::Selector::parse("a[data-type=\"Link\"]").unwrap())
-//         .next()
-//         .unwrap();
+fn get_mod_id(element: &scraper::ElementRef) -> u64 {
+    // <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=450814997" data-type="Link">https://steamcommunity.com/sharedfiles/filedetails/?id=450814997</a>
+    let link = element
+        .select(&scraper::Selector::parse("a[data-type=\"Link\"]").unwrap())
+        .next()
+        .unwrap();
 
-//     let href = link.value().attr("href").unwrap();
+    let href = link.value().attr("href").unwrap();
 
-//     let id = href.split('/').last().unwrap(); // ?id=450814997
-//     let id = id.split('=').last().unwrap(); // 450814997
-//     let id = id.parse::<u64>().unwrap(); // 450814997
+    let id = href.split('/').last().unwrap(); // ?id=450814997
+    let id = id.split('=').last().unwrap(); // 450814997
+    let id = id.parse::<u64>().unwrap(); // 450814997
 
-//     id
-// }
+    id
+}
 
-// fn get_mod_name(element: &scraper::ElementRef) -> String {
-//     // <td data-type="DisplayName">RHSUSAF</td>
-//     let name = element
-//         .select(&scraper::Selector::parse("td[data-type=\"DisplayName\"]").unwrap())
-//         .next()
-//         .unwrap();
+fn get_mod_name(element: &scraper::ElementRef) -> String {
+    // <td data-type="DisplayName">RHSUSAF</td>
+    let name = element
+        .select(&scraper::Selector::parse("td[data-type=\"DisplayName\"]").unwrap())
+        .next()
+        .unwrap();
 
-//     name.text().collect::<String>()
-// }
+    name.text().collect::<String>()
+}
 
-// fn get_name(document: &scraper::Html) -> String {
-//     document
-//         .select(&scraper::Selector::parse("meta[name=\"arma:PresetName\"]").unwrap())
-//         .next()
-//         .unwrap()
-//         .value()
-//         .attr("content")
-//         .unwrap()
-//         .to_owned()
-// }
+fn get_name(document: &scraper::Html) -> String {
+    document
+        .select(&scraper::Selector::parse("meta[name=\"arma:PresetName\"]").unwrap())
+        .next()
+        .unwrap()
+        .value()
+        .attr("content")
+        .unwrap()
+        .to_owned()
+}
 
-// fn validate(document: &scraper::Html) -> bool {
-//     validate_inner(document, "arma:Type", Some("preset"))
-//         && validate_inner(document, "generator", Some("Arma 3 Launcher - https://arma3.com"))
-//         && validate_inner(document, "arma:PresetName", None)
-// }
+fn validate(document: &scraper::Html) -> bool {
+    validate_inner(document, "arma:Type", Some("preset"))
+        && validate_inner(document, "generator", Some("Arma 3 Launcher - https://arma3.com"))
+        && validate_inner(document, "arma:PresetName", None)
+}
 
-// fn validate_inner(document: &scraper::Html, name: &str, value: Option<&str>) -> bool {
-//     let Some(value) = value else {
-//         // only check for existence
-//         return document.select(&scraper::Selector::parse(&format!("meta[name=\"{}\"]", name)).unwrap()).next().is_some();
-//     };
+fn validate_inner(document: &scraper::Html, name: &str, value: Option<&str>) -> bool {
+    let Some(value) = value else {
+        // only check for existence
+        return document.select(&scraper::Selector::parse(&format!("meta[name=\"{}\"]", name)).unwrap()).next().is_some();
+    };
 
-//     // get the meta tag where name == arma:Type
-//     let Some(meta_type) = document.select(&scraper::Selector::parse(&format!("meta[name=\"{}\"]", name)).unwrap()).next() else {
-//         return false;
-//     };
+    // get the meta tag where name == arma:Type
+    let Some(meta_type) = document.select(&scraper::Selector::parse(&format!("meta[name=\"{}\"]", name)).unwrap()).next() else {
+        return false;
+    };
 
-//     // get the value of the content attribute
-//     let Some(meta_type) = meta_type.value().attr("content") else {
-//         return false;
-//     };
+    // get the value of the content attribute
+    let Some(meta_type) = meta_type.value().attr("content") else {
+        return false;
+    };
 
-//     // check if the value is equal to "Preset"
-//     if meta_type != value {
-//         return false;
-//     }
+    // check if the value is equal to "Preset"
+    if meta_type != value {
+        return false;
+    }
 
-//     true
-// }
+    true
+}
