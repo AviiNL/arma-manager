f6c5e645884430ab883c9f414a66ff521b512e00
AviiNL <me@avii.nl>
2023-07-27 11:48:12 +0200

    fixed config file select after textarea changed

--- crates/dashboard/src/components/edit_view.rs
+++ crates/dashboard/src/components/edit_view.rs
@@ -11,37 +11,36 @@ pub fn is_mounted(cx: Scope) -> impl Fn() -> bool {
 }
 
 #[component]
-pub fn EditView(cx: Scope, channel: MaybeSignal<String>) -> impl IntoView {
+pub fn EditView<F>(cx: Scope, channel: F) -> impl IntoView
+where
+    F: Fn() -> String + 'static + Clone,
+{
     let app_state = use_context::<AppState>(cx).expect("AppState to exist");
     let arma_config = app_state.config.clone();
 
-    let content = create_rw_signal(cx, String::new());
-
     let element = create_node_ref::<Textarea>(cx);
 
-    let achannel = channel.clone();
+    let initial_load = create_rw_signal(cx, true);
+
+    let c = channel.clone();
     create_effect(cx, move |_| {
-        let channel = achannel.get();
+        let channel = c();
         let arma_config = arma_config.get();
-        let new_content = match arma_config.get(&channel) {
+        let new_content = match arma_config.get(&*channel) {
             Some(c) => c.join("\n"),
             None => String::default(),
         };
-        content.set(new_content);
+        if let Some(element) = element.get() {
+            element.set_value(&new_content);
+        }
     });
 
-    let UseScrollReturn { set_y, .. } = use_scroll_with_options(
-        cx,
-        element,
-        UseScrollOptions::default().behavior(ScrollBehavior::Smooth),
-    );
-
-    let achannel = channel.clone();
-    let save = create_action(cx, move |()| {
-        let channel = achannel.get();
+    // let achannel = channel.clone();
+    let save = create_action(cx, move |channel: &String| {
+        let channel = channel.clone();
+        let content = element.get().expect("textarea to exist").value();
         async move {
             let api = app_state.api.get_untracked().expect("api to exist");
-            let content = content.get_untracked();
             api.save_config(channel, content).await;
         }
     });
@@ -50,12 +49,14 @@ pub fn EditView(cx: Scope, channel: MaybeSignal<String>) -> impl IntoView {
         <div class="relative h-full flex flex-col">
             <div class="bg-base-100 pb-2 flex-0">
                 <div class="btn-group">
-                    <button class="btn glass hover:bg-primary btn-warning btn-sm w-8 h-8" on:click=move |_| save.dispatch(())>
+                    <button class="btn glass hover:bg-primary btn-warning btn-sm w-8 h-8" on:click=move |_| save.dispatch(channel())>
                         <i class="fa fa-save"/>
                     </button>
                 </div>
             </div>
-            <textarea class="textarea log flex-1 w-full resize-none focus:outline-none shadow-inner-xl bg-transparent mt-2" on:input=move |ev| content.set(event_target_value(&ev))>{move || content.get()}</textarea>
+            <textarea
+                class="textarea log flex-1 w-full resize-none focus:outline-none shadow-inner-xl bg-transparent mt-2"
+                node_ref=element></textarea>
         </div>
     }
 }
--- crates/dashboard/src/pages/config.rs
+++ crates/dashboard/src/pages/config.rs
@@ -8,24 +8,24 @@ use super::Page;
 
 #[component]
 pub fn Config(cx: Scope) -> impl IntoView {
-    let profile = create_rw_signal(cx, "server.cfg".to_string()); // default
+    let config_file = create_rw_signal(cx, "server.cfg".to_string()); // default
 
     view! { cx,
         <div class="card w-full flex-1 p-6 bg-base-100 shadow-xl mt-2 mb-4">
             <div class="text-xl font-semibold inline-block">
                 <div class="dropdown">
                     <label class="btn gap-1 normal-case btn-ghost" tabindex="0">
-                        {move || profile.get()}
+                        {move || config_file.get()}
                         <i class="fa fa-caret-down"></i>
                     </label>
                     <ul tabindex="0" class="dropdown-content menu p-2 shadow-lg bg-base-100 rounded-box w-fit text-sm">
                         <li>
-                            <div class="flex flex-1 grow items-center" onClick="document.activeElement.blur();" on:click=move |_| profile.set("server.cfg".to_string())>
+                            <div class="flex flex-1 grow items-center" onClick="document.activeElement.blur();" on:click=move |_| config_file.set("server.cfg".to_string())>
                                 <a href="#">"server.cfg"</a>
                             </div>
                         </li>
                         <li>
-                        <div class="flex flex-1 grow items-center" onClick="document.activeElement.blur();" on:click=move |_| profile.set("profile.cfg".to_string())>
+                        <div class="flex flex-1 grow items-center" onClick="document.activeElement.blur();" on:click=move |_| config_file.set("profile.cfg".to_string())>
                             <a href="#">"profile.cfg"</a>
                         </div>
                         </li>
@@ -35,7 +35,7 @@ pub fn Config(cx: Scope) -> impl IntoView {
             <div class="divider my-2"></div>
             <div class="h-full w-full grow bg-base-200 shadow-inner">
                 <ClientOnly>
-                <EditView channel=profile.into() />
+                <EditView channel=move || config_file.get() />
                 </ClientOnly>
             </div>
         </div>
